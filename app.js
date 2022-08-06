bc7e9e19fda8cb39ef8ef0606e951872dcc7d661
Githmin Jayawardhana <githmin21@gmail.com>
2022-08-06 19:22:53 +0530

    added changing symbol on button click

--- app.js
+++ app.js
@@ -1,21 +1,37 @@
 let symbolVar = "";
 
-document.getElementById("grid").onclick = function () {
-    symbol();
-}
+
+const buttons = document.querySelectorAll('button');
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        console.log(button.id);
+        if(document.getElementById(button.id).innerHTML === "X"){
+            console.log("X is in there");
+        } else if (document.getElementById(button.id).innerHTML === "O"){
+            console.log("O is in there")
+        } else {
+            document.getElementById(button.id).innerHTML = symbolVar;
+            symbol();
+        }
+      });
+    });
 
 
 function symbol(){
-    console.log("hi");
+    console.log("symbol");
     if(symbolVar === ""){
         symbolVar = "X";
         console.log(symbolVar);
+        return symbolVar;
     } else if(symbolVar === "X"){
         symbolVar = "O"
         console.log(symbolVar);
+        return symbolVar;
     } else if(symbolVar === "O"){
         symbolVar = "X"
         console.log(symbolVar);
+        return symbolVar;
     }
+return symbolVar;
 }
 
