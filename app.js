7b8fd61da10e04c97c50ab45a6f9b76b711262b9
Githmin Jayawardhana <githmin21@gmail.com>
2022-08-06 22:43:39 +0530

    minor cleanup

--- app.js
+++ app.js
@@ -4,11 +4,10 @@ let symbolVar = "";
 const buttons = document.querySelectorAll('button');
 buttons.forEach((button) => {
     button.addEventListener('click', () => {
-        console.log(button.id);
         if(document.getElementById(button.id).innerHTML === "X"){
             console.log("X is in there");
         } else if (document.getElementById(button.id).innerHTML === "O"){
-            console.log("O is in there")
+            console.log("O is in there");
         } else {
             document.getElementById(button.id).innerHTML = symbolVar;
             symbol();
@@ -18,20 +17,15 @@ buttons.forEach((button) => {
 
 
 function symbol(){
-    console.log("symbol");
     if(symbolVar === ""){
         symbolVar = "X";
-        console.log(symbolVar);
         return symbolVar;
     } else if(symbolVar === "X"){
         symbolVar = "O"
-        console.log(symbolVar);
         return symbolVar;
     } else if(symbolVar === "O"){
         symbolVar = "X"
-        console.log(symbolVar);
         return symbolVar;
     }
-return symbolVar;
 }
 
